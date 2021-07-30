344e710cc37e106b659557fa0aae7b81f4ef6b9b
HBD-Negin <hbdnegin.2022@gmail.com>
2021-07-30 13:58:03 +0530

    Adds 21

---
+++ Problem-Solving/21_Bill_Division/README.md
@@ -0,0 +1,8 @@
+### Problem Statement
+
+---
+
+###### Bill Division
+
+![](./que1.png)
+![](./que2.png)
---
+++ Problem-Solving/21_Bill_Division/solution.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int y;
+    cin>>y;
+    if(y <= 1917){
+        if(y%4 == 0){
+            cout<<"12.09."<<y;
+        }else{
+            cout<<"13.09."<<y;
+        }
+    }else if(y == 1918){
+        cout<<"26.09.1918";
+    }else{
+        if(y%400 == 0){
+            cout<<"12.09."<<y;
+        }else if(y%4 == 0 && y%100 != 0){
+            cout<<"12.09."<<y;
+        }else{
+            cout<<"13.09."<<y;
+        }
+    }
+return 0;
+}
