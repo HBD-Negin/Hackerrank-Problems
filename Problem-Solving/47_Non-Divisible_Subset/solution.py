0dc52f238f632844876fb5fb8e1b7d7cc0631656
HBD-Negin <hbdnegin.2022@gmail.com>
2022-01-14 10:14:12 +0530

    Adds solution to Q47

---
+++ Problem-Solving/47_Non-Divisible_Subset/solution.py
@@ -0,0 +1,21 @@
+# explanation : https://www.thepoorcoder.com/hackerrank-non-divisible-subset-solution/
+
+from collections import Counter
+
+def nonDivisibleSubset(a, k):
+    c = Counter((i%k for i in a))
+    count = 0
+    blacklist = set()
+    for x,y in c.most_common():
+        if x == k/2 or x == 0:
+            count += 1
+        elif k-x not in blacklist:
+            count += y
+            blacklist.add(x)
+    return count
+
+n, k = map(int, input().split())
+a = list(map(int, input().split()))
+
+print(nonDivisibleSubset(a, k))
+
