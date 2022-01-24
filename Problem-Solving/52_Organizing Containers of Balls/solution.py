fd444200ea44c9ac40dd83ecf3ba1bb6cdfec9df
HBD-Negin <hbdnegin.2022@gmail.com>
2022-01-24 10:14:12 +0530

    Adds solution to Q52

---
+++ Problem-Solving/52_Organizing Containers of Balls/solution.py
@@ -0,0 +1,26 @@
+q = int(input().strip())
+for a0 in range(q):
+    n = int(input().strip())
+    M = []
+    for M_i in range(n):
+       M_t = [int(M_temp) for M_temp in input().strip().split(' ')]
+       M.append(M_t)
+
+    Rows = []
+    Cols = []
+    Works = True
+    
+    for i in range(n):
+        Rows.append(sum(M[i]))
+       
+            
+    N = [list(i) for i in zip(*M)]     
+    for i in range(n):
+        Cols.append(sum(N[i]))
+  
+    Rows.sort()
+    Cols.sort()
+    if(Rows == Cols):
+        print("Possible")
+    else:
+        print("Impossible")
