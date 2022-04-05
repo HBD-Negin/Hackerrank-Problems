32bf05864ef810e3418aca317ef59d9546179b7f
HBD-Negin <hbdnegin.2022@gmail.com>
2022-04-05 10:14:12 +0530

    Adds solution to Q81

---
+++ Problem-Solving/81_Insertion_Sort - Part 1/solution.py
@@ -0,0 +1,15 @@
+def insertionSort1(n, arr):
+    target = arr[-1]
+    idx = n-2
+
+    while (target < arr[idx]) and (idx >= 0):
+        arr[idx+1] = arr[idx]
+        print(*arr)
+        idx -= 1
+
+    arr[idx+1] = target
+    print(*arr)
+
+n = int(input().strip())
+arr = list(map(int, input().rstrip().split()))
+insertionSort1(n, arr)
