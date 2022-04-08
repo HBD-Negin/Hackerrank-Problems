0247720be84c969bafe93810ac45772a5dd1b878
HBD-Negin <hbdnegin.2022@gmail.com>
2022-04-08 10:14:12 +0530

    Adds solution to Q87

---
+++ Problem-Solving/87_Mars_Exploration/solution.py
@@ -0,0 +1,13 @@
+S = input().strip()
+
+num_errors = 0
+
+for i, char in enumerate(S):
+    if i%3 == 1:
+        if char != 'O':
+            num_errors += 1
+    else:
+        if char != 'S':
+            num_errors += 1
+  
+print(num_errors)
